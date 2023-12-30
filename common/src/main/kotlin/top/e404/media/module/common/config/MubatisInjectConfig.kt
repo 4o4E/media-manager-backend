e640a0066ef2da9171e997b2eabb44fd391dd39c
4o4E <869951226@qq.com>
2023-12-31 02:40:44 +0800

    fix: 修复使用kotlin反射时获取的类型是基础类型导致mybatis-plus注入失败的问题

---
+++ common/src/main/kotlin/top/e404/media/module/common/config/MubatisInjectConfig.kt
@@ -0,0 +1,40 @@
+package top.e404.media.module.common.config
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler
+import com.baomidou.mybatisplus.core.handlers.StrictFill
+import org.apache.ibatis.reflection.MetaObject
+import org.springframework.stereotype.Component
+import top.e404.media.module.common.advice.currentUser
+
+@Component
+class MybatisColumnsHandler : MetaObjectHandler {
+    val longType = Long::class.javaObjectType
+    val boolType = Boolean::class.javaObjectType
+    override fun insertFill(metaObject: MetaObject) {
+        val currentUserId = if (InitRunner.inInitialize) 0 else currentUser!!.user.id!!
+        val now = System.currentTimeMillis()
+        val tableInfo = findTableInfo(metaObject)
+        val list: List<StrictFill<*, *>> = arrayListOf(
+            StrictFill("createBy", longType) { currentUserId },
+            StrictFill("updateBy", longType) { currentUserId },
+
+            StrictFill("createTime", longType) { now },
+            StrictFill("updateTime", longType) { now },
+
+            StrictFill("version", longType) { 0L },
+            StrictFill("deleted", boolType) { false },
+        )
+        strictInsertFill(tableInfo, metaObject, list)
+    }
+
+    override fun updateFill(metaObject: MetaObject) {
+        val currentUserId = if (InitRunner.inInitialize) 0 else currentUser!!.user.id!!
+        val now = System.currentTimeMillis()
+        val tableInfo = findTableInfo(metaObject)
+        val list: List<StrictFill<*, *>> = arrayListOf(
+            StrictFill("updateBy", longType) { currentUserId },
+            StrictFill("updateTime", longType) { now },
+        )
+        strictInsertFill(tableInfo, metaObject, list)
+    }
+}
