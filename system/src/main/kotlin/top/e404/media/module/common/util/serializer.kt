47fc3193a89ab7bc9fafe949a55bdb067c5686c5
4o4E <869951226@qq.com>
2024-09-05 15:54:38 +0800

    feat: primitive添加参数默认值

--- system/src/main/kotlin/top/e404/media/module/common/util/serializer.kt
+++ system/src/main/kotlin/top/e404/media/module/common/util/serializer.kt
@@ -4,5 +4,5 @@ import kotlinx.serialization.KSerializer
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 
-inline fun <reified T> KSerializer<T>.primitive(kind: PrimitiveKind) =
+inline fun <reified T> KSerializer<T>.primitive(kind: PrimitiveKind = PrimitiveKind.STRING) =
     PrimitiveSerialDescriptor(T::class.java.name, kind)
