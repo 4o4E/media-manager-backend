5be7597a0a2171525ab07ad4a91c6cab431d510c
4o4E <869951226@qq.com>
2024-09-13 10:59:44 +0800

    feat: 忽略日志和数据文件

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("jvm") version "2.0.20"
     kotlin("plugin.spring") version "2.0.20"
     kotlin("plugin.serialization") version "2.0.20"
+    idea
 }
 
 allprojects {
@@ -27,4 +28,10 @@ dependencies {
     implementation(project(":system"))
     implementation(project(":media"))
     implementation(project(":log4jdbc"))
+}
+
+idea {
+    module.excludeDirs.add(file(".run/logs"))
+    module.excludeDirs.add(file(".run/data"))
+    module.excludeDirs.add(file(".kotlin"))
 }
